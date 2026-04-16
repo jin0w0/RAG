39cd0c227c1ee1d80a03872a6d5d3323421e4559
jin0w0 <hwangjiin0709@gmail.com>
2026-04-16 08:08:36 +0000

    initial commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 @st.cache_resource
 def process_pdf():
-    loader = PyPDFLoader("../data/2024_KB_부동산_보고서_최종.pdf")
+    loader = PyPDFLoader("2024_KB_부동산_보고서_최종.pdf")
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     return text_splitter.split_documents(documents)
